56909ea6fb705752f1dc50f5cba9ab22353fb14c
Arthi <arthi.s@hornbillfx.com>
2020-02-27 18:26:52 +0530

    PhotonSerialize

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -12,12 +12,13 @@ public class Movement : MonoBehaviour
     void Update()
     {
         if (PhotonNetwork.isMasterClient) {
-        if (view.ownerId == 0) {
-            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-            transform.Rotate(0, x, 0);
-            transform.Translate(0, 0, z);
+            if (view.ownerId == 0) {
+                var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+                var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+                transform.Rotate(0, x, 0);
+                transform.Translate(0, 0, z);
+            }
         }
-        }
+       
     }
 }
